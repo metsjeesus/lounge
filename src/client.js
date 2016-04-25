adeb7782f5c40f7a03a311b75beea133ea1aa097
Maxime Poulin <maxpoulin64@gmail.com>
2016-04-24 22:52:31 -0400

    Settings overhaul
    This redoes pretty much everything related to client-side settings and unties them from the DOM. Most of the action happens in the `Settings` class in preparation for a future client-side API, which makes listening to setting changes a lot easier. Setting handlers never touches or sees the DOM.
    
    It also makes the binding glue between the settings and the DOM work the other way, so invisible settings are possible and can be tied to multiple controls or sources at once. This could be used later, for example, by a `/set` command or for any other reason.
    
    Communication with the server is also made separate for good measures, so the settings are usable without any active socket.
    
    A similar API is exposed on the server through `client.settings`, with the same separation of concerns, and will update all clients. These settings can be further used by the server to save preferences server-side, such as an highlight word list, auto away on/off, and many other things.

--- src/client.js
+++ src/client.js
@@ -10,6 +10,7 @@ var Msg = require("./models/msg");
 var Network = require("./models/network");
 var ircFramework = require("irc-framework");
 var Helper = require("./helper");
+var Settings = require("./clientSettings");
 
 module.exports = Client;
 
@@ -72,6 +73,7 @@ function Client(manager, name, config) {
 		name: name,
 		networks: [],
 		sockets: manager.sockets,
+		settings: new Settings(),
 		manager: manager
 	});
 
@@ -81,6 +83,10 @@ function Client(manager, name, config) {
 		client.updateToken(function(token) {
 			client.manager.updateUser(client.name, {token: token});
 		});
+
+		if ("settings" in config) {
+			this.settings.replace(config.settings);
+		}
 	}
 
 	var delay = 0;
@@ -94,6 +100,8 @@ function Client(manager, name, config) {
 	if (client.name) {
 		log.info(`User ${colors.bold(client.name)} loaded`);
 	}
+
+	this.settings.bindToClient(this);
 }
 
 Client.prototype.emit = function(event, data) {
