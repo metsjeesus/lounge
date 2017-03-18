9c5e86c8fcfc3ddf3c49c5c00d8f27a70d82737b
Pavel Djundik <github@xpaw.me>
2017-03-18 10:35:17 +0200

    Add bonuspunkt's parser

---
+++ client/js/libs/handlebars/ircmessageparser/merge.js
@@ -0,0 +1,37 @@
+const anyIntersection = require("./anyIntersection");
+const fill = require("./fill");
+
+function Object_assign(target) {
+	Array.prototype.slice.call(arguments, 1).forEach(function(obj) {
+		Object.keys(obj).forEach(function(key) {
+			target[key] = obj[key];
+		});
+	});
+	return target;
+}
+
+function merge(textParts, styleFragments) {
+	const cleanText = styleFragments.map(fragment => fragment.text).join("");
+
+	const allParts = textParts.concat(fill(textParts, cleanText))
+        .sort((a, b) => a.start - b.start);
+
+	return allParts.map(textPart => {
+		textPart.fragments = styleFragments
+            .filter(fragment => anyIntersection(textPart, fragment))
+            .map(fragment => {
+	const fragStart = fragment.start;
+	const start = Math.max(fragment.start, textPart.start);
+	const end = Math.min(fragment.end, textPart.end);
+
+	return Object_assign({}, fragment, {
+		start: start,
+		end: end,
+		text: fragment.text.slice(start - fragStart, end - fragStart)
+	});
+});
+		return textPart;
+	});
+}
+
+module.exports = merge;
